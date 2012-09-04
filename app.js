f186b4867803db8e450727e9494f519e5c56b856
TJ Holowaychuk <tj@vision-media.ca>
2012-09-03 20:21:32 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(express.static(__dirname + '/repos'));
+
+app.listen(3000);
+console.log('Component server listening in port 3000');
